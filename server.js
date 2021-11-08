ad6564046e4a788fcef959f3ed63427aceac8a00
Snake <chaldrich24@gmail.com>
2021-11-08 00:24:21 -0500

    add notes functionality with adding notes

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 const notes = require('./db/db.json');
+const {addNote, deleteNote} = require('./lib/notes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded( {extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuidv4();
+    req.body.id = uuidv4();
+    const note = addNote(req.body, notes);
+    res.json(note);
+});
 
-    notes.push(newNote);
-    res.json(newNote);
+app.delete('/api/notes/:id', (req, res) => {
+    const newNotes = deleteNote(req.params.id, notes);
+    res.json(newNotes);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
